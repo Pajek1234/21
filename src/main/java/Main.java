28049ab8a3e6e66eee2432947de4a01d9b7ea92a
Pajek1234 <162458983+Pajek1234@users.noreply.github.com>
2024-03-20 13:14:46 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class Main {
+  public static void main(String[] args) {
+    String str = "Hello world!";
+    char[] chars = str.toCharArray();
+    for (char c : chars) {
+      System.out.println(c);
+    }
+  }
+}
